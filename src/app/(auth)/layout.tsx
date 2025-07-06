f427418da44a81692833c94e2f7b49788372074e
Tawhid-sw <tawhidsarkar684@gmail.com>
2025-07-06 23:37:45 +0600

    chore: update package dependencies and configurations

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -36,116 +36,118 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     setBgImage(getRandomeImage(ImageItem));
   }, []);
   return (
-    <main className="flex items-center justify-between w-full h-screen bg-zinc-800 text-white">
-      {/* Left side image and logo */}
-      <div className="w-1/2 h-full">
-        <div className="w-[90%] h-full relative overflow-hidden">
-          {!isLoading && (
-            <h1
-              className={`px-8 absolute z-10 text-4xl text-center text-wrap inset-0 flex items-center justify-center leading-normal tracking-wider text-white font-bold ${merienda.className} animate__animated animate__fadeInUp`}
-            >
-              {BgImage?.tagline}
-            </h1>
-          )}
-
-          <div className="absolute top-4 left-4 z-10">
-            <Logo width={35} height={35} textSize="text-2xl" />
-          </div>
-
-          {isLoading && <Skeleton className="w-full h-full rounded-none" />}
-          {BgImage && BgImage.src ? (
-            <Image
-              src={BgImage.src}
-              alt={BgImage.alt || "Login Image"}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-              priority={true}
-              onLoadingComplete={() => setLoading(false)}
-              className={`transition-opacity duration-500 ease-in-out ${
-                isLoading ? "opacity-0" : "opacity-100"
-              } brightness-50`}
-            />
-          ) : (
-            <Skeleton className="w-full h-full rounded-none" />
-          )}
-          <p className="absolute bottom-4 left-4 z-10 text-sm text-white">
-            Uploaded by{" "}
-            <Link
-              href={BgImage?.authorLink || "/"}
-              className="underline text-blue-300"
-            >
-              {BgImage?.author}
-            </Link>
-          </p>
-        </div>
-      </div>
-
-      {/* Right side form and links */}
-      <div className="w-1/2 h-full flex items-center justify-center">
-        <div className=" w-[70%] h-full flex items-center justify-center flex-col">
-          <h1
-            className={`text-4xl font-bold mt-8 mb-10 text-center ${outfit.className}`}
-          >
-            {isLoainPage && "Welcome Back"}
-            {isRegisterPage && "Join With Us"}
-            {isForgotPage && "Forgot Password"}
-            {isVerifyPage && "Input Verification Code"}
-          </h1>
-          {children}
-          {(isLoainPage || isRegisterPage) && (
-            <>
-              <span className="text-sm w-1/2 text-nowrap text-zinc-400 my-8 flex items-center justify-center gap-2">
-                <hr className="w-1/2 border-zinc-400" /> or{" "}
-                {isLoainPage ? "login" : "register"} with
-                <hr className="w-1/2 border-zinc-400" />
-              </span>
-              <div className="flex items-center justify-between w-full">
-                <ButtonIcon
-                  icon={"flat-color-icons:google"}
-                  width={28}
-                  height={28}
-                  content="Google"
-                  onClick={() => alert("Google")}
-                />
-                <ButtonIcon
-                  icon={"logos:facebook"}
-                  width={28}
-                  height={28}
-                  content="Facebook"
-                  onClick={() => alert("Facebook")}
-                />
-                <ButtonIcon
-                  icon={"logos:x"}
-                  width={28}
-                  height={28}
-                  content="Twitter"
-                  onClick={() => alert("Twitter")}
-                />
-                <ButtonIcon
-                  icon={"logos:apple"}
-                  width={28}
-                  height={28}
-                  content="Apple"
-                  onClick={() => alert("Apple")}
-                />
-              </div>
-            </>
-          )}
-
-          {!isVerifyPage && (
-            <p className="text-sm text-zinc-400 my-8 self-start">
-              {isLoainPage
-                ? "Don't have an account?"
-                : "Already have an account?"}{" "}
-              <Link
-                href={isLoainPage ? "/register" : "/login"}
-                className="text-blue-400 underline"
+    <main className="w-full h-screen block bg-zinc-800 text-white">
+      <div className="flex items-center h-full justify-between w-full max-w-[1600px] mx-auto">
+        {/* Left side image and logo */}
+        <div className="w-1/2 h-full">
+          <div className="w-[90%] h-full relative overflow-hidden">
+            {!isLoading && (
+              <h1
+                className={`px-8 absolute z-10 text-4xl text-center text-wrap inset-0 flex items-center justify-center leading-normal tracking-wider text-white font-bold ${merienda.className} animate__animated animate__fadeInUp`}
               >
-                {isLoainPage ? "Join now" : "Login"}
+                {BgImage?.tagline}
+              </h1>
+            )}
+
+            <div className="absolute top-4 left-4 z-10">
+              <Logo width={35} height={35} textSize="text-2xl" />
+            </div>
+
+            {isLoading && <Skeleton className="w-full h-full rounded-none" />}
+            {BgImage && BgImage.src ? (
+              <Image
+                src={BgImage.src}
+                alt={BgImage.alt || "Login Image"}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                priority={true}
+                onLoadingComplete={() => setLoading(false)}
+                className={`transition-opacity duration-500 ease-in-out ${
+                  isLoading ? "opacity-0" : "opacity-100"
+                } brightness-50`}
+              />
+            ) : (
+              <Skeleton className="w-full h-full rounded-none" />
+            )}
+            <p className="absolute bottom-4 left-4 z-10 text-sm text-white">
+              Uploaded by{" "}
+              <Link
+                href={BgImage?.authorLink || "/"}
+                className="underline text-blue-300"
+              >
+                {BgImage?.author}
               </Link>
             </p>
-          )}
+          </div>
+        </div>
+
+        {/* Right side form and links */}
+        <div className="w-1/2 h-full flex items-center justify-center">
+          <div className=" w-[70%] h-full flex items-center justify-center flex-col">
+            <h1
+              className={`text-4xl font-bold mt-8 mb-10 text-center ${outfit.className}`}
+            >
+              {isLoainPage && "Welcome Back"}
+              {isRegisterPage && "Join With Us"}
+              {isForgotPage && "Forgot Password"}
+              {isVerifyPage && "Input Verification Code"}
+            </h1>
+            {children}
+            {(isLoainPage || isRegisterPage) && (
+              <>
+                <span className="text-sm w-1/2 text-nowrap text-zinc-400 my-8 flex items-center justify-center gap-2">
+                  <hr className="w-1/2 border-zinc-400" /> or{" "}
+                  {isLoainPage ? "login" : "register"} with
+                  <hr className="w-1/2 border-zinc-400" />
+                </span>
+                <div className="flex items-center justify-between w-full">
+                  <ButtonIcon
+                    icon={"flat-color-icons:google"}
+                    width={28}
+                    height={28}
+                    content="Google"
+                    onClick={() => alert("Google")}
+                  />
+                  <ButtonIcon
+                    icon={"logos:facebook"}
+                    width={28}
+                    height={28}
+                    content="Facebook"
+                    onClick={() => alert("Facebook")}
+                  />
+                  <ButtonIcon
+                    icon={"logos:x"}
+                    width={28}
+                    height={28}
+                    content="Twitter"
+                    onClick={() => alert("Twitter")}
+                  />
+                  <ButtonIcon
+                    icon={"logos:apple"}
+                    width={28}
+                    height={28}
+                    content="Apple"
+                    onClick={() => alert("Apple")}
+                  />
+                </div>
+              </>
+            )}
+
+            {!isVerifyPage && (
+              <p className="text-sm text-zinc-400 my-8 self-start">
+                {isLoainPage
+                  ? "Don't have an account?"
+                  : "Already have an account?"}{" "}
+                <Link
+                  href={isLoainPage ? "/register" : "/login"}
+                  className="text-blue-400 underline"
+                >
+                  {isLoainPage ? "Join now" : "Login"}
+                </Link>
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </main>
--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,28 +1,11 @@
 import React from "react";
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSlot,
-} from "@/components/ui/input-otp";
+import { Input } from "@/components/ui/input";
 
 const VarifyEmail = () => {
   return (
     <>
       <form action="">
-        <p className="text-sm font-semibold">One time password</p>
-        <InputOTP maxLength={6} className="rounded-none w-full">
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
-        </InputOTP>
-        <p className="text-sm text-muted-foreground mt-2">
-          Please enter the one-time password sent to your phone.
-        </p>
+        <Input type="text" placeholder="Enter OTP" />
         <button
           type="submit"
           className="mt-4 bg-blue-500 text-white px-3 py-2 rounded"
